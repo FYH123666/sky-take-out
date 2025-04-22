658d906a0fa6bab4d5139fa49c97e3243d67697e
fyh <1619394668@qq.com>
2025-04-22 10:21:12 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -104,4 +104,23 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> records=page.getResult();
         return new PageResult(total,records);
     }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+//        Employee employee=new Employee();
+//        employee.setId(id);
+//        employee.setStatus(status);
+//        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setUpdateUser(BaseContext.getCurrentId());
+        Employee employee=Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+
+    }
 }
