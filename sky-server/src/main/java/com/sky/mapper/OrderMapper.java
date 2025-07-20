5136df0b3fbe644f2422d3dd518ce89fbdca266b
fyh <1619394668@qq.com>
2025-07-20 21:33:38 +0800

    营业额数据统计业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -55,4 +56,11 @@ public interface OrderMapper {
 
     @Select("select * from orders where status = #{status} and order_time < #{time}")
     List<Orders> getByStatusAndOrderTimeLT(Integer pendingPayment, LocalDateTime time);
+
+    /**
+     * 根据状态统计订单数量
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map map);
 }
